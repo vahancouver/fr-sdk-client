734d3739914411a86c80933a7ffa8bc94b774bb7
Vahan Harutyunyan <vahan.harutyunyan@forgerock.com>
2021-12-21 21:39:26 -0500

    Login(with username/password) and logout using FRSession
    Create the most basic username-password login with the following requirements:
    With no frameworks or libraries, just create the most basic username-password collection UI with no dependencies other than the iOS SDK
    This UI will be static/hardcoded; it doesn't have to react to different callbacks
    Just use FRSession class for starting and completing the journey/tree
    Upon submission of the form, conditionally render "Success" or "Failure" depending on the response from ForgeRock
    Once logged in, display a functional logout button
    Once logged out, allow the login to be restarted

--- FR SDK/ViewController.swift
+++ FR SDK/ViewController.swift
@@ -5,15 +5,113 @@
 //  Created by vahan.harutyunyan  on 12/21/21.
 //
 
+/**
+ QUESTIONS:
+ 
+ 1. FRSession.currentSession is often nil after loggin in?
+ 
+ 
+ 2. After I login and logout one time, It returns access token even with invalid credentials..? and after that THINGS GO CRAZY
+ */
+
 import UIKit
+import FRAuth
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passswordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        //setup and initialize FR SDK
+        FRLog.setLogLevel([.network, .all])
+        do {
+            try FRAuth.start()
+            print("SDK initialized successfully")
+            updateStatus("SDK ready")
+        }
+        catch {
+            print(error)
+        }
+        
+        //check for existing session
+        if FRSession.currentSession != nil {
+            updateUI(loggedIn: true)
+            updateStatus("Current session detected")
+        } else {
+            updateUI(loggedIn: false)
+            updateStatus("Please log in")
+        }
     }
-
-
+    
+    @IBAction func loginButtonClicked(_ sender: UIButton) {
+        let username = usernameTextField.text
+        let password = passswordTextField.text
+        
+        FRSession.authenticate(authIndexValue: "UsernamePassword") { result, node, error in
+            if error != nil {
+                print(error.debugDescription)
+                self.updateStatus("Authentication Error")
+            }
+            
+            if let node = node {
+                for callback in node.callbacks {
+                    if callback.isKind(of: NameCallback.self), let nameCallBack = callback as? NameCallback {
+                        nameCallBack.setValue(username)
+                    }
+                    
+                    if callback.isKind(of: PasswordCallback.self), let passwordCallback = callback as? PasswordCallback {
+                        passwordCallback.setValue(password)
+                    }
+                }
+                
+                node.next(completion: {(token: Token?, node, error) in
+                    if error != nil {
+                        print(error.debugDescription)
+                        self.updateStatus("Login Failure")
+                    }
+                    
+                    if token != nil {
+                        self.updateStatus("Login Success")
+                        self.updateUI(loggedIn: true)
+                    }
+                })
+            }
+        }
+        
+    }
+    
+    @IBAction func logoutButtonClicked(_ sender: UIButton) {
+        FRSession.currentSession?.logout()
+        updateUI(loggedIn: false)
+        updateStatus("Logged out")
+    }
+    
+    private func updateStatus(_ status: String) {
+        DispatchQueue.main.async {
+            self.statusLabel.text = status
+        }
+    }
+    
+    private func updateUI(loggedIn: Bool) {
+        DispatchQueue.main.async {
+            self.loginButton.isEnabled = !loggedIn
+            self.logoutButton.isEnabled = loggedIn
+            self.usernameTextField.isEnabled = !loggedIn
+            self.passswordTextField.isEnabled = !loggedIn
+            
+            if !loggedIn {
+                self.usernameTextField.text = ""
+                self.passswordTextField.text = ""
+            }
+        }
+    }
+    
 }
 
--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Built using Xcode 13.2
 
 
 ## Here is the app progress:
-1. Hello World app with a Readme
+1. [Hello World app with a Readme](https://github.com/vahancouver/fr-sdk-client/tree/0fb0dc356e3913d107db7e842f876aaa908db342)
+2. Login(with username/password) and logout using FRSession
